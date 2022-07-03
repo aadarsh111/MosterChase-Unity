1dc36f758cbd55363400c7b4d269b8cd9d0e536c
aadarsh111 <aadarsh.rauniyar11@gmail.com>
2022-07-03 13:10:14 +0545

    Project

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{   
+    [HideInInspector]
+    public float speed;
+
+    private Rigidbody2D myBody;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        
+
+    }
+
+
+
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        myBody.velocity = new Vector2(speed, myBody.velocity.y);
+
+    }
+    
+        
+    
+}
